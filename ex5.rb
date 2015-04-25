b0f0efb1c1a915caf5ede37053ea8d1c3c1872f1
TRENDAFILOV <maxime.trendafilov@gmail.com>
2015-04-25 18:16:40 +0200

    Fini les exercices 1 à 7

---
+++ ex5.rb
@@ -0,0 +1,17 @@
+my_name = 'Zed A. Shaw'
+my_age = 35 # not a lie in 2009
+my_height = 74 # inches
+my_weight = 180 # lbs
+my_eyes = 'Blue'
+my_teeth = 'White'
+my_hair = 'Brown'
+
+puts "Let's talk about #{my_name}."
+puts "He's #{74} inches tall."
+puts "He's #{180} pounds heavy."
+puts "Actually that's not too heavy."
+puts "He's got#{Blue} eyes and #{Brown} hair."
+puts "His teeth are usually #{White} depending on the cofee."
+
+#this line is tricky, try to get it exactly right
+puts "If I add #{35}, #{74}, and #{180} I get #{35 + 74 + 180}."
